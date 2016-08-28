8ce64ee047822ea0b5f6213723c41c0cf7d5bbf1
shiro-muramatsu <maggysihro0213@gmail.com>
2016-08-28 20:56:43 +0900

    args

--- egit-test/src/EgitMain.java
+++ egit-test/src/EgitMain.java
@@ -4,6 +4,7 @@ public class EgitMain {
     public static void main(String[] args) {
         // TODO 自動生成されたメソッド・スタブ
         System.out.println("hello git");
+        System.out.println(args);
         System.exit(0);
     }
 
